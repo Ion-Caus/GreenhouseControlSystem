bc794914c6df88b11a3548bbf8ff43fec502b6af
GeraltWitcher1 <denis22@inbox.lv>
2022-05-21 21:01:38 +0200

    fixed configuration mutex according to updated reqs

--- GreenhouseControlSystem/ThresholdConfiguration.c
+++ GreenhouseControlSystem/ThresholdConfiguration.c
@@ -8,73 +8,98 @@
 #include <stdint.h>
 #include "ATMEGA_FreeRTOS.h"
 #include <semphr.h>
+#include <limits.h>
 
-static int16_t temperatureThreshold;
-static uint16_t humidityThreshold;
-static uint16_t CO2Threshold;
+static int16_t temperatureThresholdUpper;
+static int16_t temperatureThresholdLower;
+static uint16_t CO2ThresholdUpper;
+static uint16_t CO2ThresholdLower;
 
 SemaphoreHandle_t lock;
 
 void createThresholdConfig(){
 	lock = xSemaphoreCreateMutex();	
+	temperatureThresholdLower = SHRT_MIN;
+	temperatureThresholdUpper = SHRT_MAX;
+	CO2ThresholdLower = 0;
+	CO2ThresholdUpper = USHRT_MAX;
 }
 
-int16_t getTemperatureThreshold(){
+int16_t getTemperatureThresholdUpper(){
 	
 	int16_t temp = 0;
 	
 	if(xSemaphoreTake(lock, portMAX_DELAY) == pdTRUE){
-		temp = temperatureThreshold;
+		temp = temperatureThresholdUpper;
 	}
 	
 	xSemaphoreGive(lock);
 	return temp;
 }
 
-uint16_t getHumidityThreshold(){
+int16_t getTemperatureThresholdLower(){
 	
-	uint16_t hum = 0;
+	int16_t temp = 0;
 	
 	if(xSemaphoreTake(lock, portMAX_DELAY) == pdTRUE){
-		hum = humidityThreshold;
+		temp = temperatureThresholdLower;
 	}
 	
 	xSemaphoreGive(lock);
-	return hum;
+	return temp;
 }
 
-uint16_t getCo2Threshold(){
+uint16_t getCo2ThresholdUpper(){
 	uint16_t co2 = 0;
 	
 	if(xSemaphoreTake(lock, portMAX_DELAY) == pdTRUE){
-		co2 = CO2Threshold;
+		co2 = CO2ThresholdUpper;
 	}
 	
 	xSemaphoreGive(lock);
 	return co2;
 }
 
-void setTempThreshold(int16_t tempThreshold){
+uint16_t getCo2ThresholdLower(){
+	uint16_t co2 = 0;
 	
 	if(xSemaphoreTake(lock, portMAX_DELAY) == pdTRUE){
-		temperatureThreshold = tempThreshold;
+		co2 = CO2ThresholdLower;
+	}
+	
+	xSemaphoreGive(lock);
+	return co2;
+}
+
+void setTempThresholdUpper(int16_t tempThreshold){
+	
+	if(xSemaphoreTake(lock, portMAX_DELAY) == pdTRUE){
+		temperatureThresholdUpper = tempThreshold;
 	}
 	xSemaphoreGive(lock);
 }
 
-void setHumThreshold(uint16_t humThreshold){
+void setTempThresholdLower(int16_t tempThreshold){
 	
 	if(xSemaphoreTake(lock, portMAX_DELAY) == pdTRUE){
-		humidityThreshold = humThreshold;
+		temperatureThresholdLower = tempThreshold;
 	}
 	xSemaphoreGive(lock);
-	
 }
-void setCo2Threshold(uint16_t co2Threshold){
+
+
+void setCo2ThresholdUpper(uint16_t co2Threshold){
 	
 	if(xSemaphoreTake(lock, portMAX_DELAY) == pdTRUE){
-		CO2Threshold = co2Threshold;
+		CO2ThresholdUpper = co2Threshold;
 	}
 	xSemaphoreGive(lock);
+}
+
+void setCo2ThresholdLower(uint16_t co2Threshold){
 	
+	if(xSemaphoreTake(lock, portMAX_DELAY) == pdTRUE){
+		CO2ThresholdLower = co2Threshold;
+	}
+	xSemaphoreGive(lock);
 }
--- GreenhouseControlSystem/ThresholdConfiguration.h
+++ GreenhouseControlSystem/ThresholdConfiguration.h
@@ -11,9 +11,11 @@
 #include <stdint.h>
 
 void create();
-int16_t getTemperatureThreshold();
-uint16_t getHumidityThreshold();
-uint16_t getCo2Threshold();
-void setTempThreshold(int16_t tempThreshold);
-void setHumThreshold(uint16_t humThreshold);
-void setCo2Threshold(uint16_t co2Threshold);
+int16_t getTemperatureThresholdUpper();
+int16_t getTemperatureThresholdLower();
+uint16_t getCo2ThresholdUpper();
+uint16_t getCo2ThresholdLower();
+void setTempThresholdUpper(int16_t tempThreshold);
+void setTempThresholdLower(int16_t tempThreshold);
+void setCo2ThresholdUpper(uint16_t co2Threshold);
+void setCo2ThresholdLower(uint16_t co2Threshold);
